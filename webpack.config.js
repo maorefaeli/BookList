3c66a46e244d62f8f107f0ed90387b8d485e5bc7
= <maorre1@gmail.com>
2017-01-24 12:16:53 +0200

    last changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 // const webpack = require('webpack'); // to access built-in plugins
 
 module.exports = {
-    entry: ['./src/index.jsx'],
-    output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015', 'stage-1']
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './'
-    }
-    // plugins: [new webpack.optimize.UglifyJsPlugin()]
+  entry: ['./src/index.jsx'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  },
+  // plugins: [new webpack.optimize.UglifyJsPlugin()],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
